7bbee77f0a4f24ee592285a3e55e0d8d98bb6f24
Yareg <yaregr@gmail.com>
2021-05-14 22:05:54 +0300

    first

---
+++ JS/main.js
@@ -0,0 +1,31 @@
+
+const test = () => {
+    const ingridients = [
+      { id: 'e88', name: 'картошка', alergen: false, kkal: 100 },
+      { id: '08a', name: 'лук', alergen: true, kkal: 200 },
+      { id: 'a0b', name: 'шкварки', alergen: false, kkal: 300 },
+      { id: '0dd', name: 'бульйон', alergen: false, kkal: 400 },
+      { id: 'd12', name: 'морковка', alergen: false, kkal: 500 },
+      { id: '8a1', name: 'оливковая олия', alergen: false, kkal: 600 }
+    ];
+  
+    const result = {
+      status: 'ok',
+      ingridients, // ingridients: ingridients,
+      payload: {
+        error: 'test_fail',
+        message: 'тест не пройден'
+      }
+    }
+  
+    return result;
+    
+}
+
+const {ingridients} = test();
+
+let notAlergen = ingridients.filter (ev => ev.alergen == false);
+
+let totalCal = notAlergen.reduce((sum, val) => sum + val.kkal, 0);
+
+console.log(totalCal);
